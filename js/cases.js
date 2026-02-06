66229edaf9d6f47534556a4cef0968ceebd3c2cf
star-vae <star_vae_gen@163.com>
2026-02-06 17:16:18 +0800

    recon

--- js/cases.js
+++ js/cases.js
@@ -1,81 +1,29 @@
 // cases.js
 window.VAE_CASES = [
   {
-    id: "case01",
-    title: "Dog bark + footsteps",
-    note: "Listen to transient sharpness and high-frequency texture.",
+    id: "01/1051200.2min",
+    title: "Sing",
+    note: "Listen to human voice",
     tracks: {
-      "Ground Truth":   "audio/case01_gt.wav",
-      "Mel-VAE":   "audio/case01_gt.wav",
-      "AudioGen":   "audio/case01_baseline1.wav",
-      "&epsilon;ar-VAE":   "audio/case01_baseline2.wav",
-      "Stable Audio Open": "audio/case01_ours.wav",
-      "STAR-VAE": "audio/case01_ours.wav",
+      "Ground Truth":   "cases/recon/00/1051200.2min/gt.wav",
+      "Mel-VAE":   "cases/recon/00/1051200.2min/mel.wav",
+      "AudioGen":   "cases/recon/00/1051200.2min/audiogen.wav",
+      "&epsilon;ar-VAE":   "cases/recon/00/1051200.2min/ear.wav",
+      "Stable Audio Open": "cases/recon/00/1051200.2min/sao.wav",
+      "STAR-VAE": "cases/recon/00/1051200.2min/star.wav",
     }
   },
   {
-    id: "case01",
-    title: "Dog bark + footsteps",
-    note: "Listen to transient sharpness and high-frequency texture.",
+    id: "01/1169701.2min",
+    title: "Drum beat",
+    note: "Pay attention to the drum beat and its resonance.",
     tracks: {
-      "Ground Truth":   "audio/case01_gt.wav",
-      "Mel-VAE":   "audio/case01_gt.wav",
-      "AudioGen":   "audio/case01_baseline1.wav",
-      "&epsilon;ar-VAE":   "audio/case01_baseline2.wav",
-      "Stable Audio Open": "audio/case01_ours.wav",
-      "STAR-VAE": "audio/case01_ours.wav",
-    }
-  },
-  {
-    id: "case01",
-    title: "Dog bark + footsteps",
-    note: "Listen to transient sharpness and high-frequency texture.",
-    tracks: {
-      "Ground Truth":   "audio/case01_gt.wav",
-      "Mel-VAE":   "audio/case01_gt.wav",
-      "AudioGen":   "audio/case01_baseline1.wav",
-      "&epsilon;ar-VAE":   "audio/case01_baseline2.wav",
-      "Stable Audio Open": "audio/case01_ours.wav",
-      "STAR-VAE": "audio/case01_ours.wav",
-    }
-  },
-  {
-    id: "case01",
-    title: "Dog bark + footsteps",
-    note: "Listen to transient sharpness and high-frequency texture.",
-    tracks: {
-      "Ground Truth":   "audio/case01_gt.wav",
-      "Mel-VAE":   "audio/case01_gt.wav",
-      "AudioGen":   "audio/case01_baseline1.wav",
-      "&epsilon;ar-VAE":   "audio/case01_baseline2.wav",
-      "Stable Audio Open": "audio/case01_ours.wav",
-      "STAR-VAE": "audio/case01_ours.wav",
-    }
-  },
-  {
-    id: "case01",
-    title: "Dog bark + footsteps",
-    note: "Listen to transient sharpness and high-frequency texture.",
-    tracks: {
-      "Ground Truth":   "audio/case01_gt.wav",
-      "Mel-VAE":   "audio/case01_gt.wav",
-      "AudioGen":   "audio/case01_baseline1.wav",
-      "&epsilon;ar-VAE":   "audio/case01_baseline2.wav",
-      "Stable Audio Open": "audio/case01_ours.wav",
-      "STAR-VAE": "audio/case01_ours.wav",
-    }
-  },
-  {
-    id: "case01",
-    title: "Dog bark + footsteps",
-    note: "Listen to transient sharpness and high-frequency texture.",
-    tracks: {
-      "Ground Truth":   "audio/case01_gt.wav",
-      "Mel-VAE":   "audio/case01_gt.wav",
-      "AudioGen":   "audio/case01_baseline1.wav",
-      "&epsilon;ar-VAE":   "audio/case01_baseline2.wav",
-      "Stable Audio Open": "audio/case01_ours.wav",
-      "STAR-VAE": "audio/case01_ours.wav",
+      "Ground Truth":   "cases/recon/01/1169701.2min/gt.wav",
+      "Mel-VAE":   "cases/recon/01/1169701.2min/mel.wav",
+      "AudioGen":   "cases/recon/01/1169701.2min/audiogen.wav",
+      "&epsilon;ar-VAE":   "cases/recon/01/1169701.2min/ear.wav",
+      "Stable Audio Open": "cases/recon/01/1169701.2min/sao.wav",
+      "STAR-VAE": "cases/recon/01/1169701.2min/star.wav",
     }
   },
 ];
